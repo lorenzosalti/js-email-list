4e6b005b306aee472d1c9501f0b947f030a1d22c
lorenzosalti <lorenzosalti@gmail.com>
2025-03-13 16:27:23 +0100

    feat: get email data

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 axios.get(endpoint)
   .then((response) => {
+
     console.log(response);
+
+    const email = response.data.response;
+
+    console.log(email);
+
   })
   .catch(error => {
     console.log(error);
