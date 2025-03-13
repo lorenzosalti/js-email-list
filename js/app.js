6c4ba57ebb0e9d0132209d22f3386bd8cfb96753
lorenzosalti <lorenzosalti@gmail.com>
2025-03-13 16:19:55 +0100

    feat: axios get endpoint

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 const listElement = document.getElementById('email-list');
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+axios.get(endpoint)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+    console.log('errore!');
+  })
