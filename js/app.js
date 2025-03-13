d541e5779654617cc25269ccde633e2a8b93cbd9
lorenzosalti <lorenzosalti@gmail.com>
2025-03-13 17:18:04 +0100

    feat(app.js): code order and comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const listElement = document.getElementById('email-list');
 
-// let items = '';
-
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 createEmailList(10);
@@ -10,7 +8,7 @@ createEmailList(10);
 
 
 
-
+// funzione per creare e riempire un dato numero di list item con una email randomica
 function createEmailList(itemsNumber) {
 
   createListItems(itemsNumber);
@@ -25,66 +23,19 @@ function createEmailList(itemsNumber) {
 
         element.innerHTML = response.data.response;
 
-        // console.log(email);
-
-        // items += createListItem(email);
-
       })
       .catch(error => {
         console.log(error);
         console.log('errore!');
       })
   })
-}
+};
 
-
+// funzione per creare un dato numero di list item vuoti
 function createListItems(num) {
   let items = '';
   for (let i = 0; i < num; i++) {
     items += '<li class="l-item"></li>'
   }
   listElement.innerHTML = items;
-}
-
-
-
-
-
-// console.log(listItemElements);
-
-// for (let i = 0; i < 10; i++) {
-//   axios.get(endpoint)
-//     .then((response) => {
-
-//       console.log(response);
-
-//       const email = response.data.response;
-
-//       console.log(email);
-
-//       // items += createListItem(email);
-
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       console.log('errore!');
-//     })
-// }
-
-
-
-
-// setTimeout(() => {
-//   listElement.innerHTML = items;
-// }, 1000);
-
-
-
-
-
-
-
-// function createListItems(string) {
-//   const item = `<li>${string}</li>`;
-//   return item;
-// }
+};
